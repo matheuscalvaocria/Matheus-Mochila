56ff5d673f8aa8ccf264b88bee56b899da859cf3
matheuscalvaocria <134975238+matheuscalvaocria@users.noreply.github.com>
2023-11-13 19:46:29 -0300

    ...

--- js/main.js
+++ js/main.js
@@ -3,8 +3,26 @@ const form = document.getElementById("novoItem")
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
-    
-    console.log(evento)
-    console.log(evento.target.elements['nome'].value)
-    console.log(evento.target.elements['quantidade'].value)
+    criaElemento(evento.target.elements('nome.').value, evento.target.elements('quantidade').value)
 })
+
+function criaElemento(nome, quantidade) {
+    console.log(nome)
+    console.log(quantidade)
+
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    //<li class="item"><strong>3</strong>Meia</li>
+    const numeroItem = document.createElement('strong')
+    numeroItem.inn = quantidade
+
+    novoItem.appendChild(novoItem)
+    novoItem.innerHTML += nome
+
+    const lista = document.getElementById("lista")
+
+    lista.appendChild(novoItem)
+
+    console.log(novoItem)
+}

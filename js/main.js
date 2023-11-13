979f51788e67441438b72909446f834bd2711ff5
matheuscalvaocria <134975238+matheuscalvaocria@users.noreply.github.com>
2023-11-13 19:49:49 -0300

    .

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const form = document.getElementById("novoItem")
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
-    criaElemento(evento.target.elements('nome.').value, evento.target.elements('quantidade').value)
+    criaElemento(evento.target.elements('nome').value, evento.target.elements('quantidade').value)
 })
 
 function criaElemento(nome, quantidade) {
